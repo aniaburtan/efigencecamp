642e9538269748d54b1f55f2ea68496fae5b2b0b
aniaburtan <a.burtan@gmail.com>
2016-08-09 19:39:21 +0200

    Added events

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
-$(document).foundation();
+$(document).on('ready', function() {
+
+    var button = $("#go");
+    var avatar = $('.circleBox');
+
+    avatar.on('click', function(event) {
+        event.preventDefault();
+        alert(avatar.css('background-image'));
+    });
+
+    function sendAjax(password) {
+        if (password === "camp") {
+            $.ajax({
+                type: "post",
+                data: {
+                    login: "efi",
+                    password: "camp"
+                },
+                url: "https://efigence-camp.herokuapp.com/api/login",
+                error: function(response) {
+                    console.log(response.responseText);
+                },
+                success: function(response) {
+                    console.log(response);
+                }
+            });
+        }
+        else {
+            console.log("Błędne hasło");
+        }
+    };
+            
+   
+    button.on('click', function(event) {
+        event.preventDefault();
+        var pass = $('input').val();
+        // alert(pass);
+        sendAjax(pass);
+    });
+});
