57776df0d0212db9845ec139327014893a3b7dfa
aniaburtan <a.burtan@gmail.com>
2016-08-16 17:17:42 +0200

    Poprawki2

--- js/app.js
+++ js/app.js
@@ -9,42 +9,36 @@ $(document).on('ready', function() {
     });
 
     function sendAjax(password) {
-        if (password === "camp") {
-            $.ajax({
-                type: "post",
-                data: {
-                    login: "efi",
-                    password: "camp"
-                },
-                url: "https://efigence-camp.herokuapp.com/api/login",
-                error: function(response) {
-                    console.log(response.responseText);
-                },
-                success: function(response) {
-                    console.log(response);
-                    $('#go').click(function() {
-                    window.location.href = 'http://google.com';
-                    return false;
-                    });
-                }
-            });    
-        }
-                
-        else {
-            console.log("Błędne hasło");
-            $('#wrongPass').show();
-            $('#triangle').show();
-            $('#wrongPass').fadeOut(4000);
-            $('#triangle').fadeOut(4000);
-        }
+
+        $.ajax({
+            type: "post",
+            data: {
+                login: "efi",
+                password: password
+            },
+            url: "https://efigence-camp.herokuapp.com/api/login",
+            error: function(response) {
+                console.log(response.responseText);
+                console.log("Błędne hasło");
+                $('#wrongPass').show();
+                $('#triangle').show();
+                $('#wrongPass').fadeOut(4000);
+                $('#triangle').fadeOut(4000);
+            },
+            success: function(response) {
+                console.log(response);
+                window.location.href = 'http://google.com';
+
+            }
+        });
+
     };
-            
-   
+
+
     button.on('click', function(event) {
-       event.preventDefault();
+        event.preventDefault();
         var pass = $('input').val();
         // alert(pass);
         sendAjax(pass);
-}); 
     });
-    
+});
